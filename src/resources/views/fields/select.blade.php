f73886d8b10cd775d4ddbc7a6c1ee9d156f5f3d0
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-10-01 03:28:55 +0700

    add default value to select

--- src/resources/views/fields/select.blade.php
+++ src/resources/views/fields/select.blade.php
@@ -1,12 +1,17 @@
 <select
-    id="{{ $field['name'] }}"
-    name="{{ $field['name'] }}"
-    class="form-control"
-    @if($field['options']['required']) required @endif
-    @if($field['options']['readonly']) readonly @endif
+        id="{{ $field['name'] }}"
+        name="{{ $field['name'] }}"
+        class="form-control"
+        @if($field['options']['required']) required @endif
+        @if($field['options']['readonly']) readonly @endif
 >
     @foreach($field['values'] as $choice)
-        <option value="{{ $choice['value'] }}">{{ $choice['text'] }}</option>
+        <option
+            value="{{ $choice['value'] }}"
+            {{ $choice['value'] == $field['options']['value'] ? 'selected' : '' }}
+        >
+            {{ $choice['text'] }}
+        </option>
     @endforeach
 </select>
 @include('tekton::components.hint')
